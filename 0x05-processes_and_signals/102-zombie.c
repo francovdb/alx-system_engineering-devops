dfbf9eaae53057263aecf7dfae3d98c8b04ef801
Franco <vandenberg.fw@gmail.com>
2022-10-17 12:49:25 -0700

    11. Zombie

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,3 +8,25 @@
  *
  * Return: void
  */
+
+int main(void)
+{
+	pid_t childpid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		childpid = fork();
+		if (childpid == 0)
+		{
+			printf("Zombie process created, PID: %i\n", getpid());
+			exit(0);
+		}
+	}
+
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
